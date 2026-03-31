72946fa8868ab620845c61b88600ab4687c46f0c
leelah-jaffer <jafferleelah6@gmail.com>
2026-03-31 08:56:28 +0200

    made button ID

--- app/src/main/java/com/example/multiplicationtables/multiplicationTable.kt
+++ app/src/main/java/com/example/multiplicationtables/multiplicationTable.kt
@@ -1,22 +1,16 @@
 package com.example.multiplicationtables
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+class multiplicationTable : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val multiplyBtn = findViewById<Button>(R.id.ansBtn)
-        
+        setContentView(R.layout.activity_multiplication_table)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
